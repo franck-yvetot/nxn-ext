8de312c2111da5cf2e9cda82c132f6930d8a7e88
franck-yvetot <44170458+franck-yvetot@users.noreply.github.com>
2020-05-26 14:26:25 +0200

    added index.js

---
+++ index.js
@@ -0,0 +1,10 @@
+const objectSce = require("./object.service");
+const arraySce = require("./array.service");
+const stringSce = require("./string.service");
+
+/*
+const express = require("express");
+const app = express();
+*/
+
+module.exports = {objectSce,arraySce,stringSce}
